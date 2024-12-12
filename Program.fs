633293d2d631c943c19bccc9194fe83ee5cbf0c3
malakelsayyad <malakelsayyad@yahoo.com>
2024-12-12 19:22:30 +0200

    First commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.Windows.Forms
+open System.Drawing
+open System.IO
+open Newtonsoft.Json
+
+// Dictionary storage
+let mutable dictionary = Map.empty<string, string>
+
+// File path for dictionary storage
+let dictionaryFile = "dictionary.json"
+
+
+
+
